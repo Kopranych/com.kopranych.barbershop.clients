b906ff5944891f8fa4659aa0a3cf20855f5cafd1
Kopranych <kopranych@gmail.com>
2021-01-28 23:03:32 +0300

    temp implements for aws

--- src/main/kotlin/com/kopranych/barbershop/clients/repository/ClientsRepository.kt
+++ src/main/kotlin/com/kopranych/barbershop/clients/repository/ClientsRepository.kt
@@ -3,10 +3,9 @@ package com.kopranych.barbershop.clients.repository
 import com.kopranych.barbershop.clients.model.dao.Client
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
-import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface ClientsRepository : CrudRepository<Client, Long> {
+interface ClientsRepository {
   fun findAll(pageable: Pageable) : Page<Client>
 }
--- src/main/kotlin/com/kopranych/barbershop/clients/service/ClientsService.kt
+++ src/main/kotlin/com/kopranych/barbershop/clients/service/ClientsService.kt
@@ -1,29 +1,40 @@
 package com.kopranych.barbershop.clients.service
 
 import com.kopranych.barbershop.clients.model.dao.Client
-import com.kopranych.barbershop.clients.model.exception.NotFoundException
-import com.kopranych.barbershop.clients.repository.ClientsRepository
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
+import java.util.*
 
 @Service
-class ClientsService(private val clientsRepository: ClientsRepository) {
-  fun get(id: Long): Client {
-    return clientsRepository.findById(id)
-        .orElseThrow { NotFoundException("Not found client with id $id") }
-  }
+class ClientsService() {
+//  fun get(id: Long): Client {
+//    return clientsRepository.findById(id)
+//        .orElseThrow { NotFoundException("Not found client with id $id") }
+//  }
 
-  fun getAll(pageable: Pageable): Page<Client> {
-    return clientsRepository.findAll(pageable)
+  fun getAll(pageable: Pageable): Page<Client>? {
+//    randomSleep()
+//    return clientsRepository.findAll(pageable)
+    return null
   }
 
   fun save(client: Client) {
-    clientsRepository.save(client)
+//    clientsRepository.save(client)
   }
 
   fun delete(id: Long) {
-    clientsRepository.deleteById(id)
+//    clientsRepository.deleteById(id)
+  }
+
+  private fun randomSleep() {
+    val random = Random()
+    val randomNum: Int = random.nextInt(3 - 1 + 1) + 1
+    if (randomNum == 3) sleep()
+  }
+
+  private fun sleep(){
+    Thread.sleep(10000)
   }
 
 
