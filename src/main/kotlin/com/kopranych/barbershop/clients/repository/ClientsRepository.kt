5b24af624cab21734a9b5e9908b7793485fb0b5c
Kopranych <kopranych@gmail.com>
2021-02-04 22:10:21 +0300

    Revert "temp implements for aws"
    This reverts commit b906ff59

--- src/main/kotlin/com/kopranych/barbershop/clients/repository/ClientsRepository.kt
+++ src/main/kotlin/com/kopranych/barbershop/clients/repository/ClientsRepository.kt
@@ -3,9 +3,10 @@ package com.kopranych.barbershop.clients.repository
 import com.kopranych.barbershop.clients.model.dao.Client
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
+import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface ClientsRepository {
+interface ClientsRepository : CrudRepository<Client, Long> {
   fun findAll(pageable: Pageable) : Page<Client>
 }
